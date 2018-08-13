88d2237a30c08d7f56df5906ddb164bbe0fe71d4
mduby <mduby@broadinstitute.org>
2018-08-12 20:33:42 -0400

    E-02: added face smiling detection and loggig

--- AdvancedAndroid_Emojify_E02/app/src/main/java/com/example/android/emojify/utils/Emojifier.java
+++ AdvancedAndroid_Emojify_E02/app/src/main/java/com/example/android/emojify/utils/Emojifier.java
@@ -16,6 +16,13 @@ import com.google.android.gms.vision.face.FaceDetector;
  */
 
 public class Emojifier {
+
+    /**
+     * detects faces
+     * 
+     * @param context
+     * @param bitmap
+     */
     public static void detectFaces(Context context, Bitmap bitmap) {
         // local variables
         int numberOfFaces = 0;
@@ -43,15 +50,36 @@ public class Emojifier {
         Log.i(Emojifier.class.getName(), "Got number of faces: " + numberOfFaces);
 
         // if there are faces, log
-        if (numberOfFaces > 0) {
-            Log.i(Emojifier.class.getName(), "There were faces in the image of size: " + numberOfFaces);
-
-        } else {
+        if (numberOfFaces == 0) {
             // if no faces, show a toast message
             Toast.makeText(context, R.string.message_no_faces, Toast.LENGTH_LONG).show();
+
+        } else {
+            // get the probablity for each face
+            for (int i = 0; i < faceSparseArray.size(); i++) {
+                Face face = faceSparseArray.get(i);
+
+                // get the face probability
+                getClassifications(face);
+            }
         }
 
         // release the detector
         faceDetector.release();
+    }
+
+    /**
+     * log the probabilty of smiling, left and right eyes open
+     *
+     * @param face
+     */
+    private static void getClassifications(Face face) {
+        // local variables
+        String className = Emojifier.class.getName();
+
+        // log
+        Log.i(className, "the left eye open probability is: " + face.getIsLeftEyeOpenProbability());
+        Log.i(className, "the right eye open probability is: " + face.getIsRightEyeOpenProbability());
+        Log.i(className, "the smiling probability is: " + face.getIsSmilingProbability());
     }
 }
